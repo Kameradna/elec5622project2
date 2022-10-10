83227d7eb05f733aa6082c3417845b9d3f9bb35f
Philip Rowe <48018617+Kameradna@users.noreply.github.com>
2022-10-10 15:54:41 +1100

    Update parts.py

--- parts.py
+++ parts.py
@@ -1,5 +1,6 @@
 import torchvision
 import torch
+from torch.utils.data import Dataset
 
 def recycle(iterable): #stolen from Google Brain Big Transfer
   """Variant of itertools.cycle that does not save iterates."""
@@ -9,3 +10,24 @@ def recycle(iterable): #stolen from Google Brain Big Transfer
       
 def run_eval(args, data_loader):
   raise(NotImplementedError)
+
+  
+def project2set(Dataset):
+  def __init__(self, args, preprocess):
+    raise(NotImplementedError)
+    self.imgs
+    self.classes
+    self.preprocess
+    
+  def __getitem__(self, item):
+    return img, class
+
+  def __len__(self):
+    return len(self.imgs)
+  
+#train_loader, valid_loader, train_set, valid_set = parts.mktrainval(args, preprocess)
+
+def mktrainval(args, preprocess):
+  raise(NotImplementedError)
+  
+  .unsqueeze(0)
